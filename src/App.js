3e50939e4111210b6db7e5fe02b9856f785cbeda
Joseph Sungpil Choi <chousemath@gmail.com>
2018-06-29 12:43:50 +0900

    Start adding methods for fetching personal data from ethereum

--- src/App.js
+++ src/App.js
@@ -2,18 +2,48 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import web3 from './web3';
+import resume from './resume';
+
+const emptyBytes32 = '0x0000000000000000000000000000000000000000000000000000000000000000';
 
 class App extends Component {
+  state = {
+    manager: '',
+    users: [],
+    name: '',
+    position: ''
+  };
+
+  account() { return web3.eth.getAccounts()[0]; }
+  hexToUtf8(value) { return value === emptyBytes32 ? 'Not yet set' : web3.utils.hexToUtf8(value); }
+
+  async getName() {
+    const name = await resume.methods.getName(this.account()).call();
+    return this.hexToUtf8(name);
+  }
+
+  async getPosition() {
+    const position = await resume.methods.getPosition(this.account()).call();
+    return this.hexToUtf8(position);
+  }
+
+  async componentDidMount() {
+    const manager = await resume.methods.manager().call();
+    const users = await resume.methods.getUsers().call();
+    const name = await this.getName();
+    const position = await this.getPosition();
+    this.setState({ manager, users, name, position });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h2>Resume Contract</h2>
+        <p>This contract is managed by {this.state.manager}</p>
+        <p>Your name: {this.state.name}</p>
+        <p>Your position: {this.state.position}</p>
+        <p>There are currently {this.state.users.length} users participating in this contract</p>
+        <p>{this.users}</p>
       </div>
     );
   }
