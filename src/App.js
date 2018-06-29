05b8aea419fad04494f0ebf9b2505dc44c9f0937
Joseph Sungpil Choi <chousemath@gmail.com>
2018-06-29 14:36:25 +0900

    Add a form to update applicant name

--- src/App.js
+++ src/App.js
@@ -8,22 +8,28 @@ const emptyBytes32 = '0x00000000000000000000000000000000000000000000000000000000
 
 class App extends Component {
   state = {
+    message: '',
     manager: '',
     users: [],
     name: '',
+    newName: '',
     position: ''
   };
 
-  account() { return web3.eth.getAccounts()[0]; }
+  async account() {
+    const accounts = await web3.eth.getAccounts();
+    console.log('accounts:', accounts);
+    return accounts[0];
+  }
   hexToUtf8(value) { return value === emptyBytes32 ? 'Not yet set' : web3.utils.hexToUtf8(value); }
 
   async getName() {
-    const name = await resume.methods.getName(this.account()).call();
+    const name = await resume.methods.getName(await this.account()).call();
     return this.hexToUtf8(name);
   }
 
   async getPosition() {
-    const position = await resume.methods.getPosition(this.account()).call();
+    const position = await resume.methods.getPosition(await this.account()).call();
     return this.hexToUtf8(position);
   }
 
@@ -35,13 +41,49 @@ class App extends Component {
     this.setState({ manager, users, name, position });
   }
 
+  onSubmitNewName = async (event) => {
+    event.preventDefault();
+    this.setState({ message: 'Waiting on transaction...' });
+    await resume.methods.setName(web3.utils.utf8ToHex(this.state.newName)).send({ from: await this.account() });
+    this.setState({ message: 'Your new name has been successfully transmitted to the blockchain!' });
+    const name = await this.getName();
+    const users = await resume.methods.getUsers().call();
+    this.setState({ name, users });
+    setTimeout(() => {
+      this.setState({ message: '' });
+    }, 3000);
+  }
+
   render() {
+    let message;
+    if (this.state.message) {
+      message = <h3>{this.state.message}</h3>;
+    }
+
     return (
       <div>
+        {message}
         <h2>Resume Contract</h2>
+        <hr />
         <p>This contract is managed by {this.state.manager}</p>
+        <hr />
         <p>Your name: {this.state.name}</p>
+        <hr />
         <p>Your position: {this.state.position}</p>
+        <hr />
+        <form onSubmit={this.onSubmitNewName}>
+          <h3>Update your profile</h3>
+          <div>
+            <label>Your name</label>
+            <input
+              type="text"
+              value={this.state.newName}
+              onChange={event => this.setState({ newName: event.target.value })}
+            />
+          </div>
+          <button type="submit">Save your name to the blockchain</button>
+        </form>
+        <hr />
         <p>There are currently {this.state.users.length} users participating in this contract</p>
         <p>{this.users}</p>
       </div>
