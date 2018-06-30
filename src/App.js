b813d24b9509b91dd81a2a2e8ab5d8f94997de6c
Joseph Sungpil Choi <chousemath@gmail.com>
2018-06-30 17:16:13 +0900

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ const emptyBytes32 = '0x00000000000000000000000000000000000000000000000000000000
 const waitingMessage = 'Waiting on transaction...';
 const successMessage = (attributeName) => `Your new ${attributeName} has been successfully saved on the blockchain!`;
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+const genders = ['unspecified', 'male', 'female', 'other'];
+const cities = ['unspecified', 'CityA', 'CityB', 'CityC'];
+const regions = ['unspecified', 'RegionA', 'RegionB', 'RegionC'];
 
 const styles = {
   messageStyling: {
@@ -26,32 +29,65 @@ class App extends Component {
     position: '',
     newPosition: '',
     dateOfBirth: '',
-    newDateOfBirth: 0
+    newDateOfBirth: 0,
+    email: '',
+    newEmail: '',
+    gender: '',
+    newGender: 0,
+    address: '',
+    newAddressStreet: '',
+    newAddressCity: 0,
+    newAddressRegion: 0,
+    newAddressZipcode: 0
   };
 
   async account() {
     const accounts = await web3.eth.getAccounts();
-    console.log('accounts:', accounts);
     return accounts[0];
   }
   hexToUtf8(value) { return value === emptyBytes32 ? 'Not yet set' : web3.utils.hexToUtf8(value); }
 
   async getDateOfBirth() {
     const timestamp = await resume.methods.getDateOfBirth(await this.account()).call();
-    console.log('timestamp', timestamp);
     if (timestamp === '0') return 'Not yet set';
     console.log('timestamp:', timestamp);
-    const d = new Date();
-    const n = d.getTimezoneOffset() / 60.0;
-    console.log('timezone offset:', n);
+    // const d = new Date();
+    // const n = d.getTimezoneOffset() / 60.0;
     const dateOfBirth = new Date(parseInt(timestamp) * 1000);
-    dateOfBirth.setHours(dateOfBirth.getHours() + -1 * n);
-    return `${months[dateOfBirth.getMonth()]} ${dateOfBirth.getDay()}, ${dateOfBirth.getFullYear()}`;
+    // dateOfBirth.setHours(dateOfBirth.getHours() + -1 * n);
+    return `${months[dateOfBirth.getMonth()]} / ${dateOfBirth.getFullYear()}`;
+  }
+
+  async getAddress() {
+    const street = await resume.methods.getAddressStreet(await this.account()).call();
+    const city = await resume.methods.getAddressCity(await this.account()).call();
+    const region = await resume.methods.getAddressRegion(await this.account()).call();
+    const zipcode = await resume.methods.getAddressZipcode(await this.account()).call();
+    const streetName = this.hexToUtf8(street);
+    const cityName = (city >= 0 && city <= cities.length - 1) ? cities[city] : 0;
+    const regionName = (region >= 0 && region < regions.length - 1) ? regions[region] : 0;
+    const zipcodeName = (zipcode === 0) ? 'unspecified' : zipcode;
+    return `${streetName}, ${cityName}, ${regionName}, ${zipcodeName}`;
+  }
+
+  async getEmail() {
+    const email = await resume.methods.getEmail(await this.account()).call();
+    return this.hexToUtf8(email);
+  }
+
+  async getGender() {
+    const gender = await resume.methods.getGender(await this.account()).call();
+    return genders[gender];
   }
 
   async getName() {
     const name = await resume.methods.getName(await this.account()).call();
     return this.hexToUtf8(name);
+  }
+
+  async getPhone() {
+    const phone = await resume.methods.getPhone(await this.account()).call();
+    return this.hexToUtf8(phone);
   }
 
   async getPosition() {
@@ -70,7 +106,21 @@ class App extends Component {
     const name = await this.getName();
     const position = await this.getPosition();
     const dateOfBirth = await this.getDateOfBirth();
-    this.setState({ manager, users, name, position, dateOfBirth });
+    const email = await this.getEmail();
+    const gender = await this.getGender();
+    const address = await this.getAddress();
+    const phone = await this.getPhone();
+    this.setState({
+      address,
+      dateOfBirth,
+      email,
+      gender,
+      manager,
+      name,
+      phone,
+      position,
+      users
+    });
   }
 
   onSubmitNewName = async (event) => {
@@ -80,7 +130,8 @@ class App extends Component {
     this.setState({ message: successMessage('name') });
     const name = await this.getName();
     const users = await this.getUniqueUsers();
-    this.setState({ name, users });
+    const newName = '';
+    this.setState({ name, users, newName });
     setTimeout(() => {
       this.setState({ message: '' });
     }, 3000);
@@ -93,7 +144,8 @@ class App extends Component {
     this.setState({ message: successMessage('position (job)') });
     const position = await this.getPosition();
     const users = await this.getUniqueUsers();
-    this.setState({ position, users });
+    const newPosition = '';
+    this.setState({ position, users, newPosition });
     setTimeout(() => this.setState({ message: '' }), 3000);
   }
 
@@ -116,7 +168,7 @@ class App extends Component {
   render() {
     let message;
     if (this.state.message) {
-      message = <h3 style={styles.messageStyling}>{this.state.message}</h3>;
+      message = <div className="alert alert-success" role="alert">{this.state.message}</div>;
     }
     let users;
     if (this.state.users && this.state.users.length > 0) {
@@ -124,7 +176,7 @@ class App extends Component {
     }
 
     return (
-      <div>
+      <div className="container">
         {message}
         <h2>Resume Contract</h2>
         <hr />
@@ -136,54 +188,72 @@ class App extends Component {
         <hr />
         <p>Your date of birth: {this.state.dateOfBirth}</p>
         <hr />
-
-        <form onSubmit={this.onSubmitNewName}>
-          <h3>Update your profile</h3>
-          <div>
-            <label>Your name</label>
-            <input
-              type="text"
-              value={this.state.newName}
-              onChange={event => this.setState({ newName: event.target.value })}
-            />
-          </div>
-          <button type="submit">Save your name to the blockchain</button>
-        </form>
-
+        <p>Your gender: {this.state.gender}</p>
+        <hr />
+        <p>Your address: {this.state.address}</p>
+        <hr />
+        <p>Your phone number: {this.state.phone}</p>
         <hr />
 
-        <form onSubmit={this.onSubmitNewPosition}>
-          <h3>Update your current position (job)</h3>
-          <div>
-            <label>Your current position</label>
-            <input
-              type="text"
-              value={this.state.newPosition}
-              onChange={event => this.setState({ newPosition: event.target.value })}
-            />
+        <div className="row">
+          <div className="col-md-6">
+            <form onSubmit={this.onSubmitNewName}>
+              <label htmlFor="new-name">Your full name</label>
+              <div className="input-group">
+                <input type="text"
+                  value={this.state.newName}
+                  onChange={event => this.setState({ newName: event.target.value })}
+                  className="form-control"
+                  id="new-name"
+                  placeholder="Enter your new name here..."
+                />
+                <span className="input-group-btn">
+                  <button type="submit" className="btn btn-primary">Save to the blockchain</button>
+                </span>
+              </div>
+            </form>
           </div>
-          <button>Save your position to the blockchain</button>
-        </form>
-
-        <hr />
-
-        <form onSubmit={this.onSubmitNewDateOfBirth}>
-          <h3>Update your date of birth</h3>
-          <div>
-            <label>Your date of birth</label>
-            <input
-              id="date"
-              type="date"
-              value={this.state.newDateOfBirth}
-              onChange={event => this.setState({ newDateOfBirth: event.target.value })}
-            />
+          <div className="col-md-6">
+            <form onSubmit={this.onSubmitNewPosition}>
+              <label htmlFor="new-position">Your current position (job)</label>
+              <div className="input-group">
+                <input type="text"
+                  value={this.state.newPosition}
+                  onChange={event => this.setState({ newPosition: event.target.value })}
+                  className="form-control"
+                  id="new-position"
+                  placeholder="Enter your new position here..."
+                />
+                <span className="input-group-btn">
+                  <button type="submit" className="btn btn-success">Save to the blockchain</button>
+                </span>
+              </div>
+            </form>
           </div>
-          <button>Save your date of birth on the blockchain</button>
-        </form>
-
+          <div className="col-md-6">
+            <form onSubmit={this.onSubmitNewDateOfBirth}>
+              <label htmlFor="new-dob">Your date of birth</label>
+              <div className="input-group">
+                <input type="date"
+                  value={this.state.newDateOfBirth}
+                  onChange={event => this.setState({ newDateOfBirth: event.target.value })}
+                  className="form-control"
+                  id="new-dob"
+                  placeholder="Select your date of birth..."
+                />
+                <span className="input-group-btn">
+                  <button type="submit" className="btn btn-info">Save to the blockchain</button>
+                </span>
+              </div>
+            </form>
+          </div>
+          <div className="col-md-6"></div>
+        </div>
         <hr />
-        <p>There are currently {this.state.users.length} users participating in this contract</p>
-        <p>{users}</p>
+        <div className="well well-lg">
+          <p>Currently <span className="badge">{this.state.users.length}</span> users participating</p>
+          <p>{users}</p>
+        </div>
       </div>
     );
   }
